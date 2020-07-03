fix the index overflow problem

maskrcnn_benchmark/efficientdet/loss.py
@@ -52,7 +52,7 @@ class FocalLoss(nn.Module):
             bbox_annotation = bbox_annotation[bbox_annotation[:, 4] != -1]
 
             classification = torch.clamp(classification, 1e-4, 1.0 - 1e-4)
-            print("anno",torch.max(bbox_annotation[:,4], dim=0))
+            #print("anno",torch.max(bbox_annotation[:,4], dim=0))
 
 
             if bbox_annotation.shape[0] == 0:

maskrcnn_benchmark/modeling/detector/efficientdet.py
@@ -54,7 +54,7 @@ class EfficientDet(nn.Module):
         features = self.backbone(images.tensors)
 
         if targets is not None:
-            np_targets = [torch.cat([target.bbox, target.get_field("labels").float().unsqueeze(-1)],
+            np_targets = [torch.cat([target.bbox, (target.get_field("labels").float() - 1).unsqueeze(-1)],
                                          axis=-1) for target in targets]
         else:
             np_targets = []